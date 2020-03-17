6ad79a5604654a6f42b020cc8f98fd0f53b0f7e5
LauridsA <laurids.andersen@jp.dk>
2020-03-17 17:19:12 +0100

    initial catalogue modified

--- DatabaseConsoleAccess/DatabaseConsoleAccess/Program.cs
+++ DatabaseConsoleAccess/DatabaseConsoleAccess/Program.cs
@@ -7,7 +7,7 @@ namespace DatabaseConsoleAccess
     {
         static void Main(string[] args)
         {
-            var ConnectionString = @"Server=.;Database=migration_bordas_production;Integrated Security;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True; Max Pool Size=200;";
+            var ConnectionString = @"Server=.;Initial catalog=Company;Integrated Security;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True; Max Pool Size=200;";
 
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
