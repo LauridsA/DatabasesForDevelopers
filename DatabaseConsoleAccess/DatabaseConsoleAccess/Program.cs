28383d127007704233ac0c34e5f147bf126ff969
LauridsA <laurids.andersen@jp.dk>
2020-03-17 17:17:48 +0100

    stuff

--- DatabaseConsoleAccess/DatabaseConsoleAccess/Program.cs
+++ DatabaseConsoleAccess/DatabaseConsoleAccess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace DatabaseConsoleAccess
 {
@@ -6,8 +7,13 @@ namespace DatabaseConsoleAccess
     {
         static void Main(string[] args)
         {
-            var connectionstring = @"Server=.;Database=migration_bordas_production;Integrated Security;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True; Max Pool Size=200;";
-               Console.WriteLine("Hello World!");
+            var ConnectionString = @"Server=.;Database=migration_bordas_production;Integrated Security;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=True; Max Pool Size=200;";
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+
+            }
+                Console.WriteLine("Hello World!");
         }
     }
 }
